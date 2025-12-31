39172704bbd26b2eb2718016924bf206d9e45c9f
Richard Probst <richardprobst@gmail.com>
2025-12-31 15:57:15 -0300

    Adicionar configuração da página de cadastro de clientes

--- plugin/desi-pet-shower-base_plugin/desi-pet-shower-base.php
+++ plugin/desi-pet-shower-base_plugin/desi-pet-shower-base.php
@@ -55,6 +55,7 @@ require_once DPS_BASE_DIR . 'includes/class-dps-message-helper.php';
 require_once DPS_BASE_DIR . 'includes/class-dps-phone-helper.php';
 require_once DPS_BASE_DIR . 'includes/class-dps-whatsapp-helper.php';
 require_once DPS_BASE_DIR . 'includes/class-dps-admin-tabs-helper.php';
+require_once DPS_BASE_DIR . 'includes/class-dps-clients-admin-page.php';
 require_once DPS_BASE_DIR . 'includes/class-dps-shortcodes-admin-page.php';
 require_once DPS_BASE_DIR . 'includes/class-dps-dashboard.php';
 
